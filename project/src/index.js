6797cf7af292a8bb4adc7d7224dd6f7c890144a7
Eulogizethesun <38456859+Eulogizethesun@users.noreply.github.com>
2019-05-26 14:17:15 +0800

    navigator

--- project/src/index.js
+++ project/src/index.js
@@ -15,26 +15,106 @@ service.mainFrame = <h1>This.is.the.main.frame.</h1>;
 
 function Side(props) {
     return (<ul class="sidenav">
-        <li><a class="active" href="#home">主页</a></li>
-        <li><a href="#news">股票</a></li>
-        <li><a href="#contact">用户</a></li>
-        <li><a href="#about">关于</a></li>
+        <li><a class="active" id="side1" href="#home" onClick={setclassside1}>主页</a></li>
+        <li><a class="inactive" id="side2" href="#mainframe" onClick={setclassside2}>股票</a></li>
+        <li><a class="inactive" id="side3" href="#mainframe" onClick={setclassside3}>用户</a></li>
+        <li><a class="inactive" id="side4" href="#mainframe" onClick={setclassside4}>关于</a></li>
         </ul>);
 }
-
+function setclassside1(){
+    var item1 = document.getElementById('side1');
+    var item2 = document.getElementById('side2');
+    var item3 = document.getElementById('side3');
+    var item4 = document.getElementById('side4');
+    item1.setAttribute('class', 'active');
+    item2.setAttribute('class', 'inactive');
+    item3.setAttribute('class', 'inactive');
+    item4.setAttribute('class', 'inactive');
+}
+function setclassside2(){
+    var item1 = document.getElementById('side1');
+    var item2 = document.getElementById('side2');
+    var item3 = document.getElementById('side3');
+    var item4 = document.getElementById('side4');
+    item1.setAttribute('class', 'inactive');
+    item2.setAttribute('class', 'active');
+    item3.setAttribute('class', 'inactive');
+    item4.setAttribute('class', 'inactive');
+}
+function setclassside3(){
+    var item1 = document.getElementById('side1');
+    var item2 = document.getElementById('side2');
+    var item3 = document.getElementById('side3');
+    var item4 = document.getElementById('side4');
+    item1.setAttribute('class', 'inactive');
+    item2.setAttribute('class', 'inactive');
+    item3.setAttribute('class', 'active');
+    item4.setAttribute('class', 'inactive');
+}
+function setclassside4(){
+    var item1 = document.getElementById('side1');
+    var item2 = document.getElementById('side2');
+    var item3 = document.getElementById('side3');
+    var item4 = document.getElementById('side4');
+    item1.setAttribute('class', 'inactive');
+    item2.setAttribute('class', 'inactive');
+    item3.setAttribute('class', 'inactive');
+    item4.setAttribute('class', 'active');
+}
+function setclasstop1(){
+    var item1 = document.getElementById('top1');
+    var item2 = document.getElementById('top2');
+    var item3 = document.getElementById('top3');
+    var item4 = document.getElementById('top4');
+    item1.setAttribute('class', 'active');
+    item2.setAttribute('class', 'inactive');
+    item3.setAttribute('class', 'inactive');
+    item4.setAttribute('class', 'inactive');
+}
+function setclasstop2(){
+    var item1 = document.getElementById('top1');
+    var item2 = document.getElementById('top2');
+    var item3 = document.getElementById('top3');
+    var item4 = document.getElementById('top4');
+    item1.setAttribute('class', 'inactive');
+    item2.setAttribute('class', 'active');
+    item3.setAttribute('class', 'inactive');
+    item4.setAttribute('class', 'inactive');
+}
+function setclasstop3(){
+    var item1 = document.getElementById('top1');
+    var item2 = document.getElementById('top2');
+    var item3 = document.getElementById('top3');
+    var item4 = document.getElementById('top4');
+    item1.setAttribute('class', 'inactive');
+    item2.setAttribute('class', 'inactive');
+    item3.setAttribute('class', 'active');
+    item4.setAttribute('class', 'inactive');
+}
+function setclasstop4(){
+    var item1 = document.getElementById('top1');
+    var item2 = document.getElementById('top2');
+    var item3 = document.getElementById('top3');
+    var item4 = document.getElementById('top4');
+    item1.setAttribute('class', 'inactive');
+    item2.setAttribute('class', 'inactive');
+    item3.setAttribute('class', 'inactive');
+    item4.setAttribute('class', 'active');
+}
 function Top(props) {
     return (<ul class="topnav">
-        <li><a class="active" href="#home">主页</a></li>
-        <li><a href="#news">股票</a></li>
-        <li><a href="#contact">用户</a></li>
-        <li><a href="#about">关于</a></li>
+        <li><a class="active" id="top1" href="#home" onClick={setclasstop1}>主页</a></li>
+        <li><a class="inactive" id="top2" href="#mainframe" onClick={setclasstop2}>股票</a></li>
+        <li><a class="inactive" id="top3" href="#contact" onClick={setclasstop3}>用户</a></li>
+        <li><a class="inactive" id="top4" href="#about" onClick={setclasstop4}>关于</a></li>
         </ul>);
 }
 
-
+function test() {
+}
 
 function Text(){
-    return (<div style={{padding:"1px 16px",height:"1000px"}}>
+    return (<div class="mainframe"style={{padding:"1px 16px",height:"1000px"}}>
     <h2>Fixed Full-height Side Nav</h2>
     <h3>Try to scroll this area, and see how the sidenav sticks to the page</h3>
     <p>Notice that this div element has a left margin of 25%. This is because the side navigation is set to 25% width. If you remove the margin, the sidenav will overlay/sit on top of this div.</p>
